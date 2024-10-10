178d4cfa16d11a284b79fea8c77dbbaab41bf69a
ImPADfr <peter1_49@hotmail.com>
2024-10-10 17:43:37 +0200

    Premier élément du projet

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/produit",
+    name: "ListeProduit",
+
+    component: () => import("../views/ListeProduit.vue"),
+  },
+  {
+    path: "/panier",
+    name: "PanierProduit",
+
+    component: () => import("../views/PanierProduit.vue"),
+  },
+  {
+    path: "/product-details/:id",
+    name: "DetailProduit",
+    component: () => import("../views/DetailProduit.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
